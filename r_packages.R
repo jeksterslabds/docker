250292e17a10e2d01f067e6d12fdf747b5db0dc1
Ivan Jacob Agaloos Pesigan <53183031+jeksterslabds@users.noreply.github.com>
2020-04-16 12:45:42 +0800

    Update r_packages.R

--- r_packages.R
+++ r_packages.R
@@ -1,7 +1,11 @@
+repos <- "https://cran.rstudio.com"
 source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRutils/master/R/util_txt2file.R")
 source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRutils/master/R/util_user_lib.R")
 util_user_lib()
-install.packages("remotes")
+install.packages(
+  "remotes",
+  repos = repos
+)
 install_github(
   c(
     "jalvesaq/colorout",
