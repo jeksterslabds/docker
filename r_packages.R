accae9933f478a85439cc4d56b913f4411b6824d
Ivan Jacob Agaloos Pesigan <53183031+jeksterslabds@users.noreply.github.com>
2020-04-16 17:16:09 +0800

    Update r_packages.R

--- r_packages.R
+++ r_packages.R
@@ -1,9 +1,9 @@
 getwd()
+repos <- "https://cran.rstudio.com"
+source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRutils/master/R/util_txt2file.R")
+source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRutils/master/R/util_user_lib.R")
+util_user_lib(dir = "/home/user")
 list.files()
-#repos <- "https://cran.rstudio.com"
-#source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRutils/master/R/util_txt2file.R")
-#source("https://raw.githubusercontent.com/jeksterslabds/jeksterslabRutils/master/R/util_user_lib.R")
-#util_user_lib(dir = "/home/user")
 #install.packages(
 #  "remotes",
 #  repos = repos
